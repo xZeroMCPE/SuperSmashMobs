5e986c68366a02c660eb68c90f378c3c713a2d7e
TheKnightGamer <jeanclaude_amari@hotmail.com>
2016-03-19 07:11:12 -0500

    Added files via upload

---
+++ src/tkg/MobStore.php
@@ -0,0 +1,26 @@
+<?php
+namespace tkg;
+
+class MobStore{
+    /** @var  SSM */
+    private $plugin;
+    private $mobs;
+    public function __construct(SSM $plugin){
+        $this->plugin = $plugin;
+        $this->mobs = [];
+        $mobs = json_decode(file_get_contents($this->plugin->getResourcePath() . "mobs.json"), true);
+        foreach($mobs as $id => $names){
+            foreach($names as $name) {
+                $this->mobs[$name] = $id;
+            }
+        }
+    }
+    public function getMobId($name){
+        if(isset($this->mobs[strtolower($name)])){
+            return $this->mobs[strtolower($name)];
+        }
+        else{
+            return false;
+        }
+    }
+}
